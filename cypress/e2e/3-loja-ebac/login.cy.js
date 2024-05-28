119ad87934e191ff4dec16b9131a7bfd310734cd
hmota89 <humbertogurgel12@gmail.com>
2024-05-28 19:52:34 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,19 +2,24 @@
 
 describe('Funcionalidade: Login', () => {
 
-    it('Deve fazer login com sucesso', () => {
+    before(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('hmota1212@ebac.com.br')
         cy.get('#password').type('teste@123456')
         cy.get('.woocommerce-form > .button').click()
-
+       
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, hmota1212 (não é hmota1212? Sair)')
     })
 
 
     it('Deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
-        
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('hmota1212@.com.br')
         cy.get('#password').type('teste@123456')
         cy.get('.woocommerce-form > .button').click()
@@ -24,7 +29,6 @@ describe('Funcionalidade: Login', () => {
     });
 
     it.only('Deve exibir uma mensagem de erro de senha inválida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('hmota1212@ebac.com.br')
         cy.get('#password').type('teste@')
         cy.get('.woocommerce-form > .button').click()
